aa57b83fdcfc47d0d2b4617d7a0c6fe2ecc64f8e
piotr-ost <63755291+piotr-ost@users.noreply.github.com>
2020-06-15 17:51:50 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 from binance.client import Client
 from pprint import pprint
 
-api_key = 'DikaAhK0geQrUjJB7SPVIhFBlLmUR3eCoaVUUtweU8ngKsTVhUDD837o3RJinS6P'
-api_secret = 'HiXin3BZUo91rd8GV8TgswF3rrEu9wTiiYfwZ53Xqc50J3lIDB2DTQCVFSPYk5xK'
+api_key = ':)'
+api_secret = ''
 client = Client(api_key=api_key,api_secret=api_secret)
 
